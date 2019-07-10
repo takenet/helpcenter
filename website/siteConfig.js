42fbefd04d4cb0d863d506ac63838d47e51136cd
brunaags <brunazevedoguimaraes@gmail.com>
2019-07-10 10:38:24 -0300

    Fix API header link.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,7 +40,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'concepts/builder/builder-visao-geral-variaveis', label: 'Artigos'},
-    {doc: 'doc4', label: 'API'},
+    {doc: 'api', label: 'API'},
     {page: 'help', label: 'Help'},
     {blog: true, label: 'Blog'},
   ],
