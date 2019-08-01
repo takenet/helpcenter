cdb8011d5648c904563bcafa54b4043c6032e0c3
brunocamarda <bruno_camarda@hotmail.com>
2019-08-01 17:07:21 -0300

    fix aprenda mais home section

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -5,13 +5,141 @@
  * LICENSE file in the root directory of this source tree.
  */
 const React = require('react');
-
+// const { Cardx } = require ('./card.js');
 const CompLibrary = require('../../core/CompLibrary.js');
 
 
 const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
+
+// const { makeStyles } = require('@material-ui/core/styles');
+// const clsx = require('clsx');
+// const Card = require('@material-ui/core/Card');
+// const CardHeader = require ('@material-ui/core/CardHeader');
+// const CardMedia =  require('@material-ui/core/CardMedia');
+// const CardContent = require ('@material-ui/core/CardContent');
+// const CardActions =  require('@material-ui/core/CardActions');
+// const Collapse =  require('@material-ui/core/Collapse');
+// const Avatar = require('@material-ui/core/Avatar');
+// const IconButton = require ('@material-ui/core/IconButton');
+// const Typography = require ('@material-ui/core/Typography');
+// const { red } = require ('@material-ui/core/colors');
+// // const FavoriteIcon = require ('@material-ui/icons/Favorite');
+// // const ShareIcon = require ('@material-ui/icons/Share');
+// // const ExpandMoreIcon = require ('@material-ui/icons/ExpandMore');
+// // const MoreVertIcon = require ('@material-ui/icons/MoreVert');
+
+// class Cardx extends React.Component{
+// render(){
+// const useStyles = makeStyles(theme => ({
+//   card: {
+//     maxWidth: 345,
+//   },
+//   media: {
+//     height: 0,
+//     paddingTop: '56.25%', // 16:9
+//   },
+//   expand: {
+//     transform: 'rotate(0deg)',
+//     marginLeft: 'auto',
+//     transition: theme.transitions.create('transform', {
+//       duration: theme.transitions.duration.shortest,
+//     }),
+//   },
+//   expandOpen: {
+//     transform: 'rotate(180deg)',
+//   },
+//   avatar: {
+//     backgroundColor: red[500],
+//   },
+// }));
+
+//   const classes = useStyles();
+//   const [expanded, setExpanded] = React.useState(false);
+
+//   function handleExpandClick() {
+//     setExpanded(!expanded);
+//   }
+  
+//   return (
+//     <Card className={classes.card}>
+//       <CardHeader
+//         avatar={
+//           <Avatar aria-label="recipe" className={classes.avatar}>
+//             R
+//           </Avatar>
+//         }
+//         action={
+//           <IconButton aria-label="settings">
+//             <MoreVertIcon />
+//           </IconButton>
+//         }
+//         title="Shrimp and Chorizo Paella"
+//         subheader="September 14, 2016"
+//       />
+//       <CardMedia
+//         className={classes.media}
+//         image="/static/images/cards/paella.jpg"
+//         title="Paella dish"
+//       />
+//       <CardContent>
+//         <Typography variant="body2" color="textSecondary" component="p">
+//           This impressive paella is a perfect party dish and a fun meal to cook together with your
+//           guests. Add 1 cup of frozen peas along with the mussels, if you like.
+//         </Typography>
+//       </CardContent>
+//       <CardActions disableSpacing>
+//         <IconButton aria-label="add to favorites">
+//           <FavoriteIcon />
+//         </IconButton>
+//         <IconButton aria-label="share">
+//           <ShareIcon />
+//         </IconButton>
+//         <IconButton
+//           className={clsx(classes.expand, {
+//             [classes.expandOpen]: expanded,
+//           })}
+//           onClick={handleExpandClick}
+//           aria-expanded={expanded}
+//           aria-label="show more"
+//         >
+//           <ExpandMoreIcon />
+//         </IconButton>
+//       </CardActions>
+//       <Collapse in={expanded} timeout="auto" unmountOnExit>
+//         <CardContent>
+//           <Typography paragraph>Method:</Typography>
+//           <Typography paragraph>
+//             Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+//             minutes.
+//           </Typography>
+//           <Typography paragraph>
+//             Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+//             heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+//             browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+//             and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+//             pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+//             saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+//           </Typography>
+//           <Typography paragraph>
+//             Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+//             without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+//             medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+//             again without stirring, until mussels have opened and rice is just tender, 5 to 7
+//             minutes more. (Discard any mussels that don’t open.)
+//           </Typography>
+//           <Typography>
+//             Set aside off of the heat to let rest for 10 minutes, and then serve.
+//           </Typography>
+//         </CardContent>
+//       </Collapse>
+//     </Card>
+//   );
+// }
+// }
+// }
+// module.exports = Cardx;
 
 //BLiP Icon for SVGs
 const defs = '/img/defs.svg';
@@ -319,54 +447,150 @@ class Index extends React.Component {
         <p className="title"> Aprenda mais sobre o universo dos contatos inteligentes </p>
         <p className="subtitle"> Todas as informações que você precisa para saber para utilizar o BLiP como um expert estão em nas categorias abaixo: </p>
           <div className="articles">
-            
-            <div className="cards">
-            <p className="titles"> Primeiro acesso </p>
+          
+          <div className="cards">
+          <label htmlFor="check1" className="titles">Primeiros acessos
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check1" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
              
-            <div className="cards">
-            <p className="titles"> Canais </p>
+          <div className="cards">
+          <label htmlFor="check2" className="titles">Canais
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check2" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
 
-            <div className="cards">
-            <p className="titles"> Integrações</p>
+          <div className="cards">
+          <label htmlFor="check3" className="titles">Integrações
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check3" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
 
-            <div className="cards">
-            <p className="titles"> Recursos </p>
+          <div className="cards">
+          <label htmlFor="check4" className="titles">Recursos
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check4" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
 
-            <div className="cards">
-            <p className="titles"> Explore o BLiP </p>
+          <div className="cards">
+          <label htmlFor="check5" className="titles">Explore o BLiP
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check5" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
 
-            <div className="cards">
-            <p className="titles"> Cursos </p>
+          <div className="cards">
+          <label htmlFor="check6" className="titles">Cursos
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check6" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
 
-            <div className="cards">
-            <p className="titles"> Comunidade </p>
+          <div className="cards">
+          <label htmlFor="check7" className="titles">Comunidade
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check7" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div>
 
-            <div className="cards">
-            <p className="titles"> Políticas </p>
+          <div className="cards">
+          <label htmlFor="check8" className="titles">Políticas
             <BlipIcon name="arrow-ball-down-outline" className="bp-fs-3-articles bp-fill-bot"></BlipIcon>
-            <hr className="bp-divider-h--articles"></hr>
-            </div>
+          </label>
+          <input className="hidden-checkbox" id="check8" type="checkbox"></input>
+          <div className="hidden-content">
+              <ul>
+                <li><a href="#">Primeiro link </a></li>
+                <li><a href="#">Segundo link </a></li>
+                <li><a href="#">Terceiro link </a></li>
+                <li><a href="#">Quarto link </a></li>
+                <li><a href="#">Quinto link </a></li>
+                <li><a href="#">Sexto link </a></li>
+              </ul>
+          </div>
+          <hr className="bp-divider-h--articles"></hr>
+          </div> 
         
           </div>
       </div>
