5ca0e92febb9fd2d4873751e3190a83e2bc92536
brunocamarda <bruno_camarda@hotmail.com>
2019-10-17 16:40:35 -0300

    Add templates page

---
+++ website/pages/en/templates.js
@@ -0,0 +1,162 @@
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+//BLiP Icon for SVGs
+const defs = '/img/defs.svg';
+class BlipIcon extends React.Component {
+    render() {
+        const className = 'blip-icon' + (this.props.className ? (' ' + this.props.className) : '')
+        return (<svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            className={className}
+            width="1em" height="1em"
+        >
+            <use xlinkHref={`${defs}#${this.props.name}`} />
+        </svg>
+        );
+    }
+}
+
+
+class Template extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+            <div>
+
+                {/* Topo */}
+                <div>
+                    <div className="pages-top-templates">
+                        <div className="main-templates">
+                            <p className="title-templates">Modelos de chatbot</p>
+                            <p className="subtitle-templates">Modelos criados pela comunidade para facilitar a criação e implementação de chatbots em sua empresa!</p>
+                        </div>
+                        <div className="image-templates">
+                            <img className="img" src="/img/illustrations/watson-logo.png" />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Conteudo */}
+                <div className="pages-content-templates">
+                    <div className="navigation">
+                        <p className="from">BLiP Help Center ></p>
+                        <p className="from"><a className="link-from" href="/resources">Recursos</a> ></p>
+                        <p className="here">Modelos de chatbot</p>
+                    </div>
+                    <div className="info-templates">
+                        <p className="intro-templates">Os modelos de chatbot auxiliam os usuários a criar mais facilmente os fluxos de conversa,
+                      eles servem para agilizar o processo de criação dos bots no BLiP.</p>
+                    </div>
+
+                    <div className="banner-templates-section">
+                        <img className="banner-img" src="/img/illustrations/banner_templates.png"></img>
+                        <div className="banner-content">
+                            <p className="banner-text">Quer ver o seu modelo aqui também? <b>Mande pra gente!</b></p>
+                            <button disabled={true} className="banner-button">Enviar modelo</button>
+                        </div>
+                    </div>
+
+
+
+                    {/* Cards */}
+                    <div className="cards-templates">
+                        <div className="card-templates">
+                            <img className="thumb-card-templates" src="/img/illustrations/thumbs-templates/template-1.svg"></img>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Petshop</button>
+                                <p className="title-card-templates">Titulo do template aqui!</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="seeMore-card-templates">
+                                    <p className="text">Ver detalhes</p>
+                                    <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="card-templates">
+                            <img className="thumb-card-templates" src="/img/illustrations/thumbs-templates/template-2.svg"></img>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Petshop</button>
+                                <p className="title-card-templates">Titulo do template aqui!</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="seeMore-card-templates">
+                                    <p className="text">Ver detalhes</p>
+                                    <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="card-templates">
+                            <img className="thumb-card-templates" src="/img/illustrations/thumbs-templates/template-3.svg"></img>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Petshop</button>
+                                <p className="title-card-templates">Titulo do template aqui!</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="seeMore-card-templates">
+                                    <p className="text">Ver detalhes</p>
+                                    <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="card-templates">
+                            <img className="thumb-card-templates" src="/img/illustrations/thumbs-templates/template-4.svg"></img>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Petshop</button>
+                                <p className="title-card-templates">Titulo do template aqui!</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="seeMore-card-templates">
+                                    <p className="text">Ver detalhes</p>
+                                    <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="card-templates">
+                            <img className="thumb-card-templates" src="/img/illustrations/thumbs-templates/template-5.svg"></img>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Petshop</button>
+                                <p className="title-card-templates">Titulo do template aqui!</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="seeMore-card-templates">
+                                    <p className="text">Ver detalhes</p>
+                                    <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div className="card-templates">
+                            <img className="thumb-card-templates" src="/img/illustrations/thumbs-templates/template-6.svg"></img>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Petshop</button>
+                                <p className="title-card-templates">Titulo do template aqui!</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="seeMore-card-templates">
+                                    <p className="text">Ver detalhes</p>
+                                    <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+        );
+    }
+}
+
+Template.title = 'Modelos de Chatbots';
+module.exports = Template; 
