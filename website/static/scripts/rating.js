04176190076715bd31b036bdee96df379b0e0064
caioqazz <caioqazz@gmail.com>
2020-04-09 12:55:29 -0300

    Add redirect forum message into all articles

--- website/static/scripts/rating.js
+++ website/static/scripts/rating.js
@@ -1,2 +1,3 @@
 let url = parent.document.URL;
-document.write('<iframe  src="https://blip-rating-article.netlify.com?url=' + url + '" class="frame-rating"></iframe>');
+document.write('<iframe  src="https://blip-rating-article.netlify.com?url=' + url + '" class="frame-rating"></iframe>'+
+'<p class="forum-redirect-message">Não encontrou o que buscava? <a href="https://forum.blip.ai/" target="_blank">Solicite no nosso fórum!</a></p>');
