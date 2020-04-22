888ccf2c16f8203703c548b4da565c4fa0bb19ac
brunocamarda <bruno_camarda@hotmail.com>
2020-04-22 15:10:33 -0300

    create plugin page

---
+++ website/pages/en/plugins.js
@@ -0,0 +1,147 @@
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+//BLiP Icon for SVGs
+const defs = '/img/defs.svg';
+class BlipIcon extends React.Component {
+    render() {
+        const className = 'blip-icon' + (this.props.className ? (' ' + this.props.className) : '')
+        return (<svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            className={className}
+            width="1em" height="1em"
+        >
+            <use xlinkHref={`${defs}#${this.props.name}`} />
+        </svg>
+        );
+    }
+}
+
+
+class Template extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+            <div className="templates-page">
+
+                {/* Topo */}
+                <div>
+                    <div className="pages-top-templates">
+                        <div className="main-templates">
+                            <p className="title-templates">Plugins para o BLiP</p>
+                            <p className="subtitle-templates">Plugins para auxiliar na construção e evolução do seu chatbot!</p>
+                        </div>
+                        <div className="image-plugin">
+                            <img className="img" src="/img/illustrations/plugin-help-center.svg" />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Conteudo */}
+                <div className="pages-content-templates">
+                    <div className="navigation">
+                        <p className="from">BLiP Help Center ></p>
+                        <p className="from"><a className="link-from" href="/resources">Recursos</a> ></p>
+                        <p className="here">Plugins para o BLiP</p>
+                    </div>
+                    <div className="info-templates">
+                        <p className="intro-templates">Evidentemente, a expansão dos 
+                            mercados mundiais promove a alavancagem dos modos de operação convencionais</p>
+                    </div>
+
+                    <div className="banner-templates-section">
+                        <img className="banner-img" src="/img/illustrations/banner_templates.png"></img>
+                        <div className="banner-content">
+                            <p className="banner-text">Quer ter o seu plugin disponível por aqui? <b>Mande pra gente!</b></p>
+                            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSexU2elo1NSjnrsh8Et8f4MRG2X-3RSLa-Yrbq0gGqNCert1g/viewform?usp=sf_link"><button className="banner-button">Enviar plugin</button></a>
+                        </div>
+                    </div>
+
+
+
+                    {/* Cards */}
+                    <div className="cards-templates">
+                        <div className="card-templates">
+                            <div className="thumb-card-templates">
+                                <img className="thumb-image" src="/img/illustrations/thumbs-templates/petshop-agendamento.jpg"></img>
+                            </div>
+                            <div className="text-card-templates">
+                                <button className="category-card-templates">Serviços</button>
+                                <p className="title-card-templates">Petshop - Agendamento</p>
+                                <p className="creator-card-templates">criado por BLiP</p>
+                                <div className="card-footer">
+                                    <div className="rw-ui-container" name="petshop2" data-title="Petshop - Agendamento"></div>
+                                    <a href="/template?Id=Petshop_agendamento"><div className="seeMore-card-templates">
+                                        <p className="text-seeMore">Ver detalhes</p>
+                                        <BlipIcon name="arrow-ball-right-solid" className="bp-fs-4-articles bp-fill-bot"></BlipIcon>
+                                    </div></a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+                <ScriptRating></ScriptRating>
+            </div>
+        );
+    }
+}
+
+class ScriptRating extends React.Component {
+    render() {
+        return (
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+        (function(d, t, e, m){
+        
+        console.log(this);
+        // Async Rating-Widget initialization.
+        window.RW_Async_Init = function(){
+                    
+            RW.init({
+            huid: "455562",
+            uid: "885b63212d353553845c2fd806312995",
+            source: "website",
+            options: {
+                "boost": {
+                    "rate": 4
+                },
+                "readOnly": true,
+                "lng": "pt",
+                "size": "tiny",
+                "style": "flat_yellow",
+                "isDummy": false
+            } 
+        });
+            RW.render();
+        };
+            // Append Rating-Widget JavaScript library.
+        var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+            l = d.location, ck = "Y" + t.getFullYear() + 
+            "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+            f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+            a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+        if (d.getElementById(id)) return;              
+        rw = d.createElement(e);
+        rw.id = id; rw.async = true; rw.type = "text/javascript";
+        rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+        s.parentNode.insertBefore(rw, s);
+        }(document, new Date(), "script", "rating-widget.com/"));
+        `
+                }}
+            />
+        );
+    }
+}
+
+Template.title = 'Templates de Chatbots';
+module.exports = Template; 
