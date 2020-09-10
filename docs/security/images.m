0b968df404faf19c7f07b7734f4f0f711188acb2
marianastorto <68651366+marianastorto@users.noreply.github.com>
2020-09-10 10:08:23 -0300

    Create images.m

---
+++ docs/security/images.m
@@ -0,0 +1,124 @@
+---
+id: images
+title: Política de upload de mídias no BLiP
+sidebar_label: Política de upload de mídias no BLiP
+---
+
+Última atualização em setembro de 2020
+
+
+### Introdução
+Sobre os aspectos de upload e tratamento de arquivos dentro do BLiP, foram liberadas recentemente três novidades, relacionadas a:
+
+* verificação de vírus;
+
+* validação de *media types*;
+
+* tamanho máximo do arquivo.
+
+
+### Verificação de vírus
+Ao enviar um arquivo, o usuário tem o nome conferido e, usando o ClamAv, é realizada uma verificação sobre a presença de algum vírus, trojan ou malware.
+
+### Validação de *media types*
+Os arquivos suportados pelo BLiP estão listados abaixo:
+
+* audio/aac
+
+* audio/midi
+
+* audio/mp3
+
+* audio/mp4
+
+* audio/mpeg
+
+* audio/wav
+
+* audio/ogg
+
+* audio/wma
+
+* audio/webm
+
+* audio/opus
+
+* audio/x-aac
+
+* image/gif
+
+* image/jpeg
+
+* image/jpg
+
+* image/jfif
+
+* image/png
+
+* image/svg+xml
+
+* image/tiff
+
+* image/vnd.dwg
+
+* image/webp
+
+* video/3gpp
+
+* video/avi
+
+* video/mpeg
+
+* video/mpg
+
+* video/mp4
+
+* video/mov
+
+* video/m4v
+
+* video/wmv
+
+* video/webm
+
+* video/ogg
+
+* application/pdf
+
+* application/vnd.ms-excel
+
+* application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+
+* application/vnd.openxmlformats-officedocument.spreadsheetml.template
+
+* application/msword
+
+* application/vnd.openxmlformats-officedocument.wordprocessingml.document
+
+* application/vnd.openxmlformats-officedocument.wordprocessingml.template
+
+* application/vnd.ms-powerpoint
+
+* application/vnd.ms-outlook
+
+* application/vnd.openxmlformats-officedocument.presentationml.presentation
+
+* application/vnd.openxmlformats-officedocument.presentationml.template
+
+* application/vnd.openxmlformats-officedocument.presentationml.slideshow
+
+* application/zip
+
+* application/x-zip-compressed
+
+* application/x-rar-compressed
+
+* text/csv
+
+* text/html
+
+* text/plain
+
+### Tamanho máximo do arquivo
+
+O tamanho máximo do arquivo aceito pela platafora BLiP é de 20MB.
